c963253643c8770b4433a6ebe0bb36d8822a5575
asuslaptop184 <88846832+asuslaptop184@users.noreply.github.com>
2021-08-12 20:40:25 +0200

    Update code.js

--- code.js
+++ code.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const ytdl = require("ytdl-core");
 const app = express();
-// setting the port
+
 let port=process.env.PORT || 5000;
 
 app.use(express.json());
